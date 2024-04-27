4880e1de7761e0e6e559621fca4fa7cbfd5bf08e
sabbir-islam <sabbir3921@gmail.com>
2024-04-27 19:33:58 +0600

    "commit"

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.home', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
